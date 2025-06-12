94b60b01babd79c6c0a1fc503b7bed96f66c654d
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2025-06-12 13:25:07 +0900

    [FEAT] 코인 획득 기능 구현

---
+++ Assets/Scripts/Level/Coin.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private bool isRotating = true;
+    private bool isMoving = false;
+    private SphereCollider sCollider;
+    private BoxCollider bCollider;
+    private Transform player;
+    [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
+    private float moveSpeed;
+
+    void Start()
+    {
+        sCollider = GetComponent<SphereCollider>();
+        bCollider = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        if(isRotating)
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+        
+        if(isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, player.position) < 0.1f)
+                Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player")) return;
+
+        bCollider.isTrigger = true;
+        isRotating = false;
+        isMoving = true;
+        player = other.transform;
+    }
+}
